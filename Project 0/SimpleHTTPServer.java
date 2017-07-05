ac99f009d994bd838753f9c48b32877df65c0823
Alex Smirnov <ays45@rutgers.edu>
2017-07-05 18:31:16 -0400

    added doc_root and TODO in server file

--- Project 0/SimpleHTTPServer.java
+++ Project 0/SimpleHTTPServer.java
@@ -17,7 +17,18 @@ public static void main(String[] args){
 		client = server.accept();
 		out = new PrintWriter(client.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-		
+		/* TODO: When client connects to ServerSocket, spawn a new thread to handle 
+		 communication. No reading or writing to or from a Socket should occur in 
+		 the class that the ServerSocket is accepting connections in. 
+		 
+		 In the thread, read a single String from the client, parse it as an 
+		 HTTP 0.8 request and send back an appropriate response according to 
+		 the HTTP 0.8 protocol below.
+		 
+		 Once response has been sent, you should flush() your output streams, wait 
+		 a quarter second, close down all communication objects and exit thread.*/
+		 
+		 
 	}
 	catch(Exception e){
 		System.out.println(e);
