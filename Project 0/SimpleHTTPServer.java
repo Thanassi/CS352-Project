139930810b7c965c13ebe5b247a8adf20207e113
Thanassi <tnatsis@yahoo.com>
2017-07-06 00:11:14 -0400

    cleaned up some code

--- Project 0/SimpleHTTPServer.java
+++ Project 0/SimpleHTTPServer.java
@@ -1,15 +1,21 @@
-import java.net.*;
-import java.io.*;
-import java.util.concurrent.TimeUnit;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 /*
 Authors: 
 Thanassi Natsis
 Alexey Smirnov
 */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+import java.util.concurrent.TimeUnit;
 
 public class SimpleHTTPServer{
 	
@@ -139,9 +145,10 @@ class SimpleServerThread extends Thread{
 	// Print resource contents line by line
 	public void printResource(String path, PrintWriter out) throws IOException{
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-		   String data = "", line = null;
+		   StringBuilder data = new StringBuilder();
+		   String line;
 		   while ((line = br.readLine()) != null) {
-			   data += line;
+			   data.append(line);
 		   }
 		   out.println(data);
 		}
